0d6be1bde40da3c6eeb340cad5e68eea29041473
nefelitav <nefeli.tavoulari123@gmail.com>
2024-02-15 19:23:21 +0100

    fix github actions

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-
+import {
+  Login
+} from "./components";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Login/>,
     
   },
   {
